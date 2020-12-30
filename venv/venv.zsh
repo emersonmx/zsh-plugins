dea4657ff2d3b54731c219cd04f71666ed51d048
Emerson MX <emersonmx@gmail.com>
2020-12-30 13:41:07 -0300

    Create venv.zsh

---
+++ venv/venv.zsh
@@ -0,0 +1,9 @@
+ZSH_VENV_DIRNAME=".venv"
+
+function mkvenv() {
+    python -m venv "$ZSH_VENV_DIRNAME"
+    source "$ZSH_VENV_DIRNAME/bin/activate"
+    pip install --upgrade pip setuptools wheel
+}
+
+alias source-venv="source \"$ZSH_VENV_DIRNAME/bin/activate\""
