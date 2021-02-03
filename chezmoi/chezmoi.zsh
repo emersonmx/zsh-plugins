829e6bb11b4c0dad5ed9dcfefc7a2785d8752f5b
Emerson Max de Medeiros Silva <emerson.silva@invillia.com>
2021-02-03 19:37:36 -0300

    chezmoi: Rename alias to use cm instead cz

--- chezmoi/chezmoi.zsh
+++ chezmoi/chezmoi.zsh
@@ -4,17 +4,17 @@ then
     return 1
 fi
 
-alias cz="chezmoi"
-alias czcd="chezmoi cd"
-alias cza="chezmoi add"
-alias cze="chezmoi edit"
-alias czec="chezmoi edit-config"
-alias czdf="chezmoi diff"
-alias czap="chezmoi apply"
-alias czme="chezmoi merge"
-alias czca="chezmoi chattr"
-alias czma="chezmoi managed"
-alias czum="chezmoi unmanaged"
+alias cm="chezmoi"
+alias cmcd="chezmoi cd"
+alias cma="chezmoi add"
+alias cme="chezmoi edit"
+alias cmec="chezmoi edit-config"
+alias cmdf="chezmoi diff"
+alias cmap="chezmoi apply"
+alias cmme="chezmoi merge"
+alias cmca="chezmoi chattr"
+alias cmma="chezmoi managed"
+alias cmum="chezmoi unmanaged"
 
 if (( $+commands[fzf] ))
 then
@@ -23,23 +23,23 @@ then
         [[ -n $managed_file ]] && echo $managed_file || return 1
     }
 
-    function fcze() {
+    function fcme() {
         chezmoi edit "$(_chezmoi_fzf)"
     }
 
-    function fczdf() {
+    function fcmdf() {
         chezmoi diff "$(_chezmoi_fzf)"
     }
 
-    function fczap() {
+    function fcmap() {
         chezmoi apply -v "$(_chezmoi_fzf)"
     }
 
-    function fczme() {
+    function fcmme() {
         chezmoi merge "$(_chezmoi_fzf)"
     }
 
-    function fczca() {
+    function fcmca() {
         chezmoi chattr "$(_chezmoi_fzf)"
     }
 fi
