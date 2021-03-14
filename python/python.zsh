114aefbc6d6561e20f745f558fc171d9169d6409
Emerson MX <emersonmx@gmail.com>
2021-03-14 12:42:27 -0300

    Rename python alias

--- python/python.zsh
+++ python/python.zsh
@@ -20,5 +20,5 @@ function mkpypkg() {
 alias source-venv="source \"$ZSH_PYTHON_VENV_DIRNAME/bin/activate\""
 alias py="python"
 alias ipy="ipython"
-alias pip-freeze="pip freeze > requirements.txt"
-alias pip-install="pip install -r requirements.txt"
+alias pfr="pip freeze > requirements.txt"
+alias pir="pip install -r requirements.txt"
