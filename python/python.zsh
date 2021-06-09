c2c33eac5da3890184fdaffebaa6b25b04e89696
Emerson Max de Medeiros Silva <emerson.silva@invillia.com>
2021-06-09 20:59:03 -0300

    Use virtualenv in mkvenv

--- python/python.zsh
+++ python/python.zsh
@@ -1,7 +1,7 @@
 ZSH_PYTHON_VENV_DIRNAME=".venv"
 
 function mkvenv() {
-    python -m venv "$ZSH_PYTHON_VENV_DIRNAME"
+    virtualenv "$ZSH_PYTHON_VENV_DIRNAME"
     source "$ZSH_PYTHON_VENV_DIRNAME/bin/activate"
     pip install --upgrade pip setuptools wheel
 }
