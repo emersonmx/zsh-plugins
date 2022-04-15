628767584cd3039ba323c25d838f7af36eedee89
Emerson MX <emersonmx@gmail.com>
2022-04-15 19:57:07 -0300

    [python] Remove unused alias

--- python/python.zsh
+++ python/python.zsh
@@ -1,22 +1,2 @@
-ZSH_PYTHON_VENV_DIRNAME=".venv"
-
-function mkvenv() {
-    virtualenv "$ZSH_PYTHON_VENV_DIRNAME"
-    source "$ZSH_PYTHON_VENV_DIRNAME/bin/activate"
-    pip install --upgrade pip setuptools wheel
-}
-
-function rmvenv() {
-    [[ $(command -v deactivate) ]] && deactivate
-    rm -rI "$ZSH_PYTHON_VENV_DIRNAME"
-}
-
-function mkpypkg() {
-    pkgname="$1"
-    mkdir -p "$pkgname"
-    touch "$pkgname/__init__.py"
-}
-
-alias source-venv="source \"$ZSH_PYTHON_VENV_DIRNAME/bin/activate\""
 alias py="python"
 alias ipy="ipython"
